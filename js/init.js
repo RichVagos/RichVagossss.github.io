f1c398919e5454e9dfe281a06bdfd2fd003ddb2a
RichVagos <87157952+RichVagos@users.noreply.github.com>
2021-07-08 22:08:00 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,9 @@
-
+firebase.initializeApp({
+    apiKey: "AIzaSyBGwT5Vaf2GCBRvHMrPu5Bq8AI_REM2pgM",
+    authDomain: "autenrichv.firebaseapp.com",
+    projectId: "autenrichv",
+    storageBucket: "autenrichv.appspot.com",
+    messagingSenderId: "357072619068",
+    appId: "1:357072619068:web:18ab87e1e64228cf8e6539",
+    measurementId: "G-82PLSHPFTS"
+});

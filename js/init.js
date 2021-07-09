01b6bf8eb57d21eae003ef0cbd036af5599cbc51
RichVagos <87157952+RichVagos@users.noreply.github.com>
2021-07-08 21:47:22 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 
 firebase.initializeApp({
-   apiKey: "AIzaSyDmovq7T_-Ym7iutIjJIN1ABI33usaMmJo",
-    authDomain: "autenfi41.firebaseapp.com",
-    projectId: "autenfi41",
-    storageBucket: "autenfi41.appspot.com",
-    messagingSenderId: "777550911881",
-    appId: "1:777550911881:web:205487691c96b111382958",
-    measurementId: "G-YHTXKL0597"
+ apiKey: "AIzaSyBGwT5Vaf2GCBRvHMrPu5Bq8AI_REM2pgM",
+    authDomain: "autenrichv.firebaseapp.com",
+    projectId: "autenrichv",
+    storageBucket: "autenrichv.appspot.com",
+    messagingSenderId: "357072619068",
+    appId: "1:357072619068:web:18ab87e1e64228cf8e6539",
+    measurementId: "G-82PLSHPFTS"
 });
